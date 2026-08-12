0925681513313fb84d7f135aac138e0e9f724e3f
Máté Horváth <jonahwhite115@gmail.com>
2026-08-13 00:16:44 +0200

    logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,33 +1,24 @@
 import logging
-import sys
+import os
+from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        self.handler = logging.StreamHandler(sys.stdout)
-        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self.handler.setFormatter(self.formatter)
-        self.logger.addHandler(self.handler)
+# Create a logger object
+logger = logging.getLogger('GameLogger')
+logger.setLevel(logging.DEBUG)
 
-    def debug(self, message):
-        self.logger.debug(message)
+# Define log file path
+log_file_path = os.path.join(os.getcwd(), 'game.log')
 
-    def info(self, message):
-        self.logger.info(message)
+# Create a rotating file handler
+handler = RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=3)
+handler.setLevel(logging.DEBUG)
 
-    def warning(self, message):
-        self.logger.warning(message)
+# Create a formatter and set it for the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
 
-    def error(self, message):
-        self.logger.error(message)
+# Add the handler to the logger
+logger.addHandler(handler)
 
-    def critical(self, message):
-        self.logger.critical(message)
-
-# Example usage
-if __name__ == '__main__':
-    log = Logger('GameLogger')
-    log.info('Game started')
-    log.error('An error occurred')
-    log.debug('Debugging information')
+# Logging example
+logger.info('Logger setup complete. Ready to log events.')
