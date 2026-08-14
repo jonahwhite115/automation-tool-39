0e815e4750c59ac2ff1e526c27d9edd08b4d2fdc
Máté Horváth <jonahwhite115@gmail.com>
2026-08-14 05:44:13 +0200

    add logger with rotation

--- logger.py
+++ logger.py
@@ -1,24 +1,28 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+# Logger setup function
 
-def setup_logger(name, log_file, level=logging.INFO):
-    """Function to set up a logger with rotation"""  
-    logger = logging.getLogger(name)  
-    logger.setLevel(level)  
+def setup_logger(log_file='app.log', max_bytes=10*1024*1024, backup_count=5):
+    # Create a logger
+    logger = logging.getLogger('GameAutomationLogger')
+    logger.setLevel(logging.DEBUG)  # Set the logging level
 
-    # Create a rotating file handler  
-    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)  
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  
-    handler.setFormatter(formatter)  
+    # Create a handler that writes log messages to a file with rotation
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    handler.setLevel(logging.DEBUG)
 
-    if not logger.handlers:
-        logger.addHandler(handler)  
+    # Create a formatter and set it for the handler
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(handler)
     return logger
 
-
-# Example usage  
-if __name__ == '__main__':  
-    my_logger = setup_logger('game_logger', 'game.log')  
-    my_logger.info('This is an info message.')  
-    my_logger.error('This is an error message.')  
+# Example usage of the logger
+if __name__ == '__main__':
+    log = setup_logger()
+    log.info('Logger initialized successfully')
+    log.warning('This is a warning message')
+    log.error('This is an error message')
